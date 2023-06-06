7353dc30077765845b189224bf5f366d253c8ba0
Théo Porzio <theo.porzio@student.junia.com>
2023-06-06 14:24:28 +0200

    Recup du bordel d'Alexcendre

--- client/src/Component/Header/Header.js
+++ client/src/Component/Header/Header.js
@@ -1,42 +1,33 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ToggleTheme from "../ToggleTheme/ToggleTheme";
+import React, {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+
 
 function Header(props) {
-    const [estConnecte, setEstConnecte] = useState(false);
+  const [estConnecte, setEstConnecte] = useState(false);
 
 
   return (
-    <header className="bg-primary text-header-footer-text p-4 mb-4">
-      <div className="flex justify-between items-center">
-        <Link to={"/"} className={"text-2xl font-bold"}>
-          Sub Aquatic Group Wattignies
-        </Link>
-        <div className={"flex space-x-4"}>
+      <header className="bg-gray-800 text-white p-4 mb-4">
+        <div className="flex justify-between items-center">
+          <Link to={"/"} className={"text-2xl font-bold"}>
+            Sub Aquatic Group Wattignies
+          </Link>
           <nav>
             <ul className="flex space-x-4 items-center">
               <li>
-                <Link
-                  to={"/diver"}
-                  className={"hover:text-header-footer-text-hover"}
-                >
+                <Link to={"/diver"} className={"hover:text-gray-300"}>
                   Diver
                 </Link>
               </li>
               <li>
-                <Link
-                  to={"/instructor"}
-                  className={"hover:text-header-footer-text-hover"}
-                >
+                <Link to={"/instructor"} className={"hover:text-gray-300"}>
                   Dive Director
                 </Link>
               </li>
             </ul>
           </nav>
-          <ToggleTheme />
         </div>
-      </div>
-    </header>
+      </header>
   );
 }
 
