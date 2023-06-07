639dbd14e8dc385bb7c4aea93d0b03ea347c6f61
Théo Porzio <theo.porzio@student.junia.com>
2023-06-07 12:31:09 +0200

    bug

--- client/src/Component/Header/Header.js
+++ client/src/Component/Header/Header.js
@@ -5,42 +5,42 @@ import ToggleTheme from "../ToggleTheme/ToggleTheme";
 
 
 function Header(props) {
-    const [estConnecte, setEstConnecte] = useState(false);
+  const [estConnecte, setEstConnecte] = useState(false);
 
-    useEffect(() => {
-        setConnected(props.connected);
-    }, [props.connected]);
+  useEffect(() => {
+    setEstConnecte(props.connected);
+  }, [props.connected]);
   return (
-    <header className="bg-primary text-header-footer-text p-4 mb-4">
-      <div className="flex justify-between items-center">
-        <Link to={"/"} className={"text-2xl font-bold"}>
-          Sub Aquatic Group Wattignies
-        </Link>
-        <div className={"flex space-x-4"}>
-          <nav>
-            <ul className="flex space-x-4 items-center">
-              <li>
-                <Link
-                  to={"/diver"}
-                  className={"hover:text-header-footer-text-hover"}
-                >
-                  Diver
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to={"/instructor"}
-                  className={"hover:text-header-footer-text-hover"}
-                >
-                  Dive Director
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <ToggleTheme />
+      <header className="bg-primary text-header-footer-text p-4 mb-4">
+        <div className="flex justify-between items-center">
+          <Link to={"/"} className={"text-2xl font-bold"}>
+            Sub Aquatic Group Wattignies
+          </Link>
+          <div className={"flex space-x-4"}>
+            <nav>
+              <ul className="flex space-x-4 items-center">
+                <li>
+                  <Link
+                      to={"/diver"}
+                      className={"hover:text-header-footer-text-hover"}
+                  >
+                    Diver
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                      to={"/instructor"}
+                      className={"hover:text-header-footer-text-hover"}
+                  >
+                    Dive Director
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <ToggleTheme />
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
   );
 }
 
