8b8e3c6db6b14c478c4a0db6436d18b5e6d726f2
Quentin <quentin.pruvot@student.yncrea.fr>
2023-06-08 17:24:39 +0200

    Planning front + dives available almost good + Diver Info

--- client/src/Component/Calendar/Calendar.js
+++ client/src/Component/Calendar/Calendar.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
     Resources,
     MonthView,
+    WeekView,
     Appointments,
     AppointmentTooltip,
     AppointmentForm,
@@ -32,9 +36,16 @@ export default class Demo extends React.PureComponent {
                     allowMultiple: true,
                 },
             ],
+            selectedEventMembers: [],
+            currentView: 'month',
+            currentDate: new Date(), // Ajout de currentDate dans l'état initial
         };
 
         this.commitChanges = this.commitChanges.bind(this);
+        this.updateEventMembers = this.updateEventMembers.bind(this);
+        this.onMemberToggle = this.onMemberToggle.bind(this);
+        this.changeView = this.changeView.bind(this);
+        this.changeToToday = this.changeToToday.bind(this); // Nouvelle fonction pour le bouton "Today"
     }
 
     commitChanges({ added, changed, deleted }) {
@@ -42,49 +53,189 @@ export default class Demo extends React.PureComponent {
             let { data } = state;
             if (added) {
                 const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-                data = [...data, { id: startingAddedId, ...added }];
+                const addedEvent = { id: startingAddedId, ...added, members: [] };
+                data = [...data, addedEvent];
                 console.log(data);
             }
             if (changed) {
-                data = data.map(appointment => (
-                    changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+                data = data.map((appointment) =>
+                    changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment
+                );
                 console.log(data);
             }
             if (deleted !== undefined) {
-                data = data.filter(appointment => appointment.id !== deleted);
+                data = data.filter((appointment) => appointment.id !== deleted);
                 console.log(data);
             }
             return { data };
         });
     }
 
+    updateEventMembers(eventId, selectedMembers) {
+        this.setState((state) => {
+            const { data } = state;
+            const updatedData = data.map((appointment) => {
+                if (appointment.id === eventId) {
+                    return { ...appointment, members: selectedMembers };
+                }
+                return appointment;
+            });
+            return { data: updatedData };
+        });
+    }
+
+    onMemberToggle(memberId) {
+        this.setState((state) => {
+            const { selectedEventMembers } = state;
+            const isSelected = selectedEventMembers.includes(memberId);
+            let updatedMembers;
+            if (isSelected) {
+                updatedMembers = selectedEventMembers.filter((id) => id !== memberId);
+            } else {
+                updatedMembers = [...selectedEventMembers, memberId];
+            }
+            return { selectedEventMembers: updatedMembers };
+        });
+    }
+
+    changeView(viewName) {
+        this.setState({ currentView: viewName });
+        if (viewName === 'month') {
+            const currentDate = new Date();
+            this.setState({ currentDate: currentDate });
+        }
+        if (viewName === 'week') {
+            const currentDate = new Date();
+            this.setState({ currentDate: currentDate });
+        }
+    }
+
+    changeToToday() {
+        const currentDate = new Date();
+        this.setState({ currentDate: currentDate });
+    }
+
     render() {
-        const { data, resources } = this.state;
+        const { data, resources, selectedEventMembers, currentView, currentDate } = this.state;
+
+        const renderMembers = ({ availableMembers, onMemberToggle }) => (
+            <div>
+                {availableMembers.map((member) => (
+                    <div key={member.id}>
+                        <input
+                            type="checkbox"
+                            checked={selectedEventMembers.includes(member.id)}
+                            onChange={() => onMemberToggle(member.id)}
+                        />
+                        <span>{member.text}</span>
+                    </div>
+                ))}
+            </div>
+        );
 
         return (
             <Paper>
-                <Scheduler
-                    data={data}
-                >
-                    <ViewState
-                        defaultCurrentDate="2017-05-25"
+                <AppBar position="static">
+                    <Toolbar>
+                        <Button
+                            color="inherit"
+                            onClick={() => this.changeView('month')}
+                            disabled={currentView === 'month'}
+                        >
+                            Month
+                        </Button>
+                        <Button
+                            color="inherit"
+                            onClick={() => this.changeView('week')}
+                            disabled={currentView === 'week'}
+                        >
+                            Week
+                        </Button>
+                        <div style={{ marginLeft: 'auto' }}>
+                            {currentView === 'month' && (
+                                <React.Fragment>
+                                    <Button
+                                        color="inherit"
+                                        onClick={() => {
+                                            let newDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1);
+                                            this.setState({ currentDate: newDate });
+                                        }}
+                                    >
+                                        &lt; Previous Month
+                                    </Button>
+                                    <Button
+                                        color="inherit"
+                                        onClick={() => {
+                                            let newDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1);
+                                            this.setState({ currentDate: newDate });
+                                        }}
+                                    >
+                                        Next Month &gt;
+                                    </Button>
+                                </React.Fragment>
+                            )}
+                            {currentView === 'week' && (
+                                <React.Fragment>
+                                    <Button
+                                        color="inherit"
+                                        onClick={() => {
+                                            let newDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 7);
+                                            this.setState({ currentDate: newDate });
+                                        }}
+                                    >
+                                        &lt; Previous Week
+                                    </Button>
+                                    <Button
+                                        color="inherit"
+                                        onClick={() => {
+                                            const newDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 7);
+                                            this.setState({ currentDate: newDate });
+                                        }}
+                                    >
+                                        Next Week &gt;
+                                    </Button>
+                                </React.Fragment>
+                            )}
+                            <Button
+                                color="inherit"
+                                onClick={() => this.changeToToday()}
+                            >
+                                Today
+                            </Button>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                <Scheduler data={data}>
+                    <ViewState currentDate={currentDate} currentViewName={currentView} />
+                    <EditingState onCommitChanges={this.commitChanges} />
+                    <EditRecurrenceMenu
+                        onFieldChange={this.updateEventMembers}
+                        renderMembers={renderMembers}
+                        onMemberToggle={this.onMemberToggle}
+                        availableMembers={resources[1].instances}
+                        selectedMembers={selectedEventMembers}
                     />
-                    <EditingState
-                        onCommitChanges={this.commitChanges}
-                    />
-                    <EditRecurrenceMenu />
-
-                    <MonthView />
+                    <MonthView name="month" />
+                    <WeekView name="week" cellDuration={120} startDayHour={6} intervalCount={1} />
                     <Appointments />
-                    <AppointmentTooltip
-                        showOpenButton
+                    <AppointmentTooltip showOpenButton />
+                    <AppointmentForm
+                        basicLayoutComponent={(props) => (
+                            <AppointmentForm.BasicLayout
+                                {...props}
+                                availableMembers={resources[1].instances}
+                                selectedMembers={selectedEventMembers}
+                            />
+                        )}
+                        textEditorComponent={(props) => (
+                            <AppointmentForm.TextEditor
+                                {...props}
+                                availableMembers={resources[1].instances}
+                                selectedMembers={selectedEventMembers}
+                            />
+                        )}
                     />
-                    <AppointmentForm />
-
-                    <Resources
-                        data={resources}
-                        mainResourceName="roomId"
-                    />
+                    <Resources data={resources} mainResourceName="roomId" />
                     <DragDropProvider />
                 </Scheduler>
             </Paper>
--- client/src/Component/DiverDivingAvailable/DiverDivingAvailable.js
+++ client/src/Component/DiverDivingAvailable/DiverDivingAvailable.js
@@ -1,43 +1,101 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const DiverDivingAvailable = () => {
-    const events = [
-        {
-            title: 'Événement 1',
-            location: 'Lieu 1',
-            description: 'Description de l\'événement 1',
-            startDate: '2023-06-01',
-            endDate: '2023-06-05',
-        },
-        {
-            title: 'Événement 2',
-            location: 'Lieu 2',
-            description: 'Description de l\'événement 2',
-            startDate: '2023-06-10',
-            endDate: '2023-06-15',
-        },
-    ];
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true); // Nouvel état pour gérer le chargement
+    const [locations, setLocations] = useState({}); // Nouvel état pour stocker les informations de location
+
+    useEffect(() => {
+        fetchEvents();
+        getLocations().then((data) => {
+            let locationsMap = {};
+            data.forEach((location) => {
+                locationsMap[location.id] = location.name;
+            });
+            setLocations(locationsMap);
+        });
+    }, []);
+
+    const fetchEvents = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/dives/all', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setEvents(data);
+                setLoading(false); // Fin du chargement des événements
+                console.log('Events:', data); // Afficher les événements dans la console
+            } else {
+                console.log('Error getting events:', response.status);
+            }
+        } catch (error) {
+            console.log('Error getting events:', error);
+        }
+    };
+
+    const getLocations = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/sites/all', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                const data = await response.json();
+                return data;
+            } else {
+                console.log('Error getting locations:', response.status);
+                return [];
+            }
+        } catch (error) {
+            console.log('Error getting locations:', error);
+            return [];
+        }
+    };
+
+    if (loading) {
+        return <p>Loading events...</p>; // Afficher un message de chargement pendant le chargement des événements
+    }
+
+    const currentDate = new Date();
+
+    const formatDateTime = (dateString) => {
+        const date = new Date(dateString);
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+        return date.toLocaleString('en-US', options);
+    };
+
+    const getLocationName = (locationId) => {
+        return locations[locationId] || ''; // Retourner le nom de la location ou une chaîne vide si le nom n'est pas trouvé
+    };
+
+    const futureEvents = events.filter(event => {
+        const eventStartDate = new Date(event.date_begin);
+        return eventStartDate > currentDate;
+    });
 
     return (
-        <div className="bg-gray-100">
+        <div className="h-full w-full m-1 bg-gray-100">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-                    <h2 className="text-2xl font-bold text-gray-900 pt-0">Plongées Disponibles</h2>
+                <div className="mx-auto max-w-2xl lg:max-w-none">
+                    <h2 className="text-2xl font-bold text-gray-900 p-5">Available Dives</h2>
 
                     <div className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
-                        {events.map((event, index) => (
+                        {futureEvents.map((event, index) => (
                             <div
                                 className="bg-white rounded-lg shadow-lg p-6 max-w-md"
                                 key={index}
                             >
-                                <h2 className="text-lg font-bold text-gray-900 mb-2">{event.title}</h2>
-                                <p className="text-gray-700 mb-4">Localisation: {event.location}</p>
-                                <p className="text-gray-700 mb-4">Description: {event.description}</p>
-                                <p className="text-gray-700 mb-4">Date de début: {event.startDate}</p>
-                                <p className="text-gray-700 mb-4">Date de fin: {event.endDate}</p>
-                                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                                    Inscription
-                                </button>
+                                <h2 className="text-lg font-bold text-gray-900 mb-2">{event.name}</h2>
+                                <p className="text-gray-700 mb-4">Location: {getLocationName(event.dive_site)}</p>
+                                <p className="text-gray-700 mb-4">Comment: {event.comment}</p>
+                                <p className="text-gray-700 mb-4">Starting Date: {formatDateTime(event.date_begin)}</p>
+                                <p className="text-gray-700 mb-4">Ending Date: {formatDateTime(event.date_end)}</p>
                             </div>
                         ))}
                     </div>
