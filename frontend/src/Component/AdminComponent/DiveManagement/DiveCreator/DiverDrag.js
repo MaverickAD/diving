c731d1d1aacada62208b32b666b4c6a4a2a27155
MaverickAD <dprzalex@gmail.com>
2023-06-22 12:50:19 +0200

    Dive Management plus Dive Creator

---
+++ frontend/src/Component/AdminComponent/DiveManagement/DiveCreator/DiverDrag.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useDrag } from "react-dnd";
+
+function DiverDrag(props) {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: "diver",
+    item: {
+      last_name: props.diver.last_name,
+      first_name: props.diver.first_name,
+      pa: props.diver.pa,
+      pe: props.diver.pe,
+      palanquee: props.diver.palanquee,
+    },
+    collect: (monitor) => ({
+      isDragging: !monitor.isDragging(),
+    }),
+  });
+  return (
+    <div
+      className={"diver-card px-1 py-2 grid grid-cols-4 border-b"}
+      ref={dragRef}
+    >
+      <p className={"text-center"}>{props.diver.first_name}</p>
+      <p className={"text-center"}>{props.diver.last_name}</p>
+      <p className={"text-center"}>{props.diver.pa}</p>
+      <p className={"text-center"}>{props.diver.pe}</p>
+    </div>
+  );
+}
+
+export default DiverDrag;
---
+++ frontend/src/Component/AdminComponent/DiveManagement/DiveCreator/PalanqueeDrop.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useDrop } from "react-dnd";
+import DiverDrag from "./DiverDrag";
+
+function PalanqueeDrop(props) {
+  const [{}, dropRef] = useDrop({
+    accept: "diver",
+    drop: (item) => {
+      props.update(
+        props.divers.map((diver) => {
+          if (
+            diver.last_name === item.last_name &&
+            diver.first_name === item.first_name &&
+            diver.pa === item.pa &&
+            diver.pe === item.pe &&
+            diver.palanquee === item.palanquee
+          ) {
+            diver.palanquee = props.palanquee;
+          }
+          return diver;
+        })
+      );
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
+  });
+
+  useEffect(() => {
+    props.setModify(
+      props.divers.filter((diver) => diver.palanquee === props.palanquee)
+        .length === 1 ||
+        props.divers.filter((diver) => diver.palanquee === props.palanquee)
+          .length > 4
+    );
+  });
+
+  return (
+    <div className={"w-full border border-black"} ref={dropRef}>
+      {props.divers
+        .filter((diver) => diver.palanquee === props.palanquee)
+        .map((diver, index) => (
+          <DiverDrag key={index} draggable diver={diver} />
+        ))}
+    </div>
+  );
+}
+
+export default PalanqueeDrop;
