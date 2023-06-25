60b584ebda34c857313b2b55a1c394ff26491165
Théo Porzio <theo.porzio@student.junia.com>
2023-06-25 15:29:47 +0200

    bug

--- frontend/src/Hooks/useVerifyToken.js
+++ frontend/src/Hooks/useVerifyToken.js
@@ -9,51 +9,52 @@ function useVerifyToken(typePage) {
     const token = localStorage.getItem("token");
 
     if (token) {
-      axios
-        .post(
-          "http://localhost:5000/api/users/verify",
-          {
-            token: localStorage.getItem("token"),
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-              "x-access-token": localStorage.getItem("token"),
-            },
-          }
-        )
-        .then((response) => {
-          if (response.status === 200) {
-            if (typePage === "admin") {
-              if (
-                !(
-                  response.data.decoded.rank === 2 ||
-                  response.data.decoded.rank === 1
-                )
-                .then((response) => {
-                    if (response.status === 200) {
-                        if(new Date(response.data.decoded.timestamp) < Date.now()) {
-                            localStorage.removeItem("token")
-                            navigate('/login');
-                        }
-                        if(typePage === "admin") {
-                            if (!(response.data.decoded.rank === 2 || response.data.decoded.rank === 1)) {
-                                navigate("/")
-                            }
-                        }
-                    } else {
-                        navigate('/login');
+        axios
+            .post(
+                "http://localhost:5000/api/users/verify",
+                {
+                    token: localStorage.getItem("token"),
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "x-access-token": localStorage.getItem("token"),
+                    },
+                }
+            )
+            .then((response) => {
+                if (response.status === 200) {
+                    if (typePage === "admin") {
+                        if (
+                            !(
+                                response.data.decoded.rank === 2 ||
+                                response.data.decoded.rank === 1
+                            )
+                                .then((response) => {
+                                    if (response.status === 200) {
+                                        if (new Date(response.data.decoded.timestamp) < Date.now()) {
+                                            localStorage.removeItem("token")
+                                            navigate('/login');
+                                        }
+                                        if (typePage === "admin") {
+                                            if (!(response.data.decoded.rank === 2 || response.data.decoded.rank === 1)) {
+                                                navigate("/")
+                                            }
+                                        }
+                                    } else {
+                                        navigate('/login');
+                                    }
+                                })
+                                .catch((error) => {
+                                    console.log(error);
+                                })) ;
                     }
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        } else {
-            navigate('/login');
-        }
-    }, [navigate]);
+                } else {
+                    navigate('/login');
+                }
+            }, [navigate]);
 
-
+    }})
 }
 
 export default useVerifyToken;
